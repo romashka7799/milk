75f75e41235428ef47246e79ab6757065a71b67c
romashka7799 <efimo997@gmail.com>
2024-11-17 14:30:35 +0300

    Update пр.py

--- практика 10/пр.py
+++ практика 10/пр.py
@@ -1,20 +1,16 @@
-# Открываем файл для чтения
 with open('C:\\Users\\Борис\\Desktop\\универ\\практика 10\\ЕАБ_уб-42_vvod.txt', 'r') as file:
-    # Читаем размеры матрицы
+    # читаем размер матрицы
     rows, cols = map(int, file.readline().split())
-    # Создаем матрицу и заполняем ее
     N = []
     for _ in range(rows):
         row = list(map(int, file.readline().split()))
         N.append(row)
         
-# Открываем файл для записи
 with open('C:\\Users\\Борис\\Desktop\\универ\\практика 10\\ЕАБ_уб-42_vivod.txt', 'w',encoding='utf-8') as file:
-    # Выводим матрицу N
     file.write("массив a[N][N]\n")
     for row in N:
         file.write(" ".join(map(str, row)) + "\n")
-    # Вычисляем сумму элементов на главной диагонали
+    #сумма элементов на главной диагонали
     s = 0
     for i in range(len(N)):
         for j in range(len(N)):
@@ -27,5 +23,5 @@ with open('C:\\Users\\Борис\\Desktop\\универ\\практика 10\\Е
         for elem in row:
             if elem > 0:
                 count += 1
-    file.write(f"положительные {count}\n")# Открываем файл для записи
+    file.write(f"положительные {count}\n")# открываем файл для записи
     
